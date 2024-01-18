0c988c3a9a00e559de8c8b7cc76e190e24295869
m020202 <m020202@naver.com>
2024-01-18 18:01:52 +0900

    프로젝트 업로드

---
+++ src/main/java/study/clipart/ClipartApplication.java
@@ -0,0 +1,13 @@
+package study.clipart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClipartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClipartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/clipart/ClipartApplicationTests.java
@@ -0,0 +1,13 @@
+package study.clipart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClipartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
